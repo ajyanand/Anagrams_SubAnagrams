b177334fcea95309aa9a78759b1a763446e0f53d
ajyanand <writetoajayanand@gmail.com>
2022-06-23 15:29:01 -0700

    git commit

---
+++ Main.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+
+# print 'Number of arguments:', len(sys.argv), 'arguments.'
+# print 'Argument List:', str(sys.argv)
+
+while True:
+    try: 
+        wordList = np.genfromtxt(sys.argv[0], dtype= 'char', comments='#', delimiter=None)
+    except Exception as e:
+        print(e)
