7d7eba45636f48950165281a76cb7e63b464d12f
ajyanand <writetoajayanand@gmail.com>
2022-06-28 11:19:11 -0700

    Solved problem, to optimize

--- Main.py
+++ Main.py
@@ -1,46 +1,68 @@
 
 from ast import Break
 import sys
-# import numpy as np
-from collections import defaultdict
+import numpy as np
+# from collections import defaultdict
 
 from cv2 import sort
 
-def load_words(filename:str) -> list:
+def loadWords(filename:str) -> list:
     with open(filename,'r') as f:
         return [ line.strip() for line in f ]
-
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-# print ('Argument List:', str(sys.argv))
+def isAnagram(item:str,word:str,)->int:  
+    i:int = 0
+    j:int = 0
+    while (i < len(word) and j < len(item)):  
+        if word[i] == item[j]:
+            i+=1
+            j+=1
+            continue
+        elif word[i] < item[j]:
+            i+=1
+            continue
+        elif word[i] > item[j]:
+            break      
+    if j == len(item):
+        return 1      
+    return 0
 x = 0
 while x == 0:    
     try: 
         fileName:str = sys.argv[1]
-        wordList = load_words(fileName)
+        wordList = loadWords(fileName)
         if len(wordList) !=0 and len(word) != 0:
             x+=1 
         word:str = sys.argv[2]  
-    except Exception as e:
-        # print(e)
+    except Exception:
         fileName:str = input('please enter the name of a file in the current directory containing the word list: ')
-        wordList = load_words(fileName)
+        wordList = loadWords(fileName)
         word:str = input('please enter the word to find anagrams and sub anagrams for: ')
         if len(wordList) !=0 and len(word) != 0:
             x+=1 
 anagrams:int = 0
-sortedWord:str = sorted(word)           
+sortedWord:str = sorted(word)       
+
 for item in wordList:
     sortedItem = sorted(item)
-    j:int = 0
-    for i in range(min(len(item),len(word))):
-        print(i)
-        if sortedItem[i] == sortedWord[i]:
-            j+=1
+    if len(item) == len(word):
+        if sortedItem == sortedWord:
+            print('matching word:',item)
+            anagrams+=1
         else:
-            Break
-    if j==len(item):
-        anagrams+=1           
+            continue
+    #TO COMPLETE            
+    # can do using dict for every letter associated with a number in the word for this as well.
+    # or can do creating list of charecters and removing them after each letter is matched from the original word.
+    # can also do using numpy, using np.where and replacing the matched letter with 0.
+    elif len(word)>len(item):
+        if isAnagram(sortedItem,sortedWord) == 1:
+           print('matching word:',item)
+           anagrams+=1
+    else: 
+        continue
+
+print('size of the word list is: ',len(wordList))                  
 print(f"The number of anagrams and sub anagrams are: {anagrams}")
 
 # getting a list of words and a single string as a arguments
-# print(wordList)
+
