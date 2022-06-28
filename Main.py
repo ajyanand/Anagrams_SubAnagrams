fa74f720ee4957301678304a14e92623ffc23433
ajyanand <writetoajayanand@gmail.com>
2022-06-28 11:37:51 -0700

    compacted different cases into function

--- Main.py
+++ Main.py
@@ -5,20 +5,32 @@ def loadWords(filename:str) -> list:
     with open(filename,'r') as f:
         return [ line.strip() for line in f ]
 def isAnagram(item:str,word:str,)->int:  
+    """
+    params: 
+    word: string to find anagrams and sub anagrams for 
+    item: string to check against
+    return:
+    int 0 if False, 1 if true 
+    """
     i:int = 0
     j:int = 0
-    while (i < len(word) and j < len(item)):  
-        if word[i] == item[j]:
-            i+=1
-            j+=1
-            continue
-        elif word[i] < item[j]:
-            i+=1
-            continue
-        elif word[i] > item[j]:
-            break      
-    if j == len(item):
-        return 1      
+    if len(item) == len(word):
+        if sortedItem == sortedWord:
+            return 1 
+    elif len(word)>len(item):
+        while (i < len(word) and j < len(item)):  
+            if word[i] == item[j]:
+                i+=1
+                j+=1
+                continue
+            elif word[i] < item[j]:
+                i+=1
+                continue
+            elif word[i] > item[j]:
+                # Since the two strings are sorted, if the next character in
+                break      
+        if j == len(item):
+            return 1      
     return 0
 x:int = 0
 while x == 0:    
@@ -38,16 +50,12 @@ anagrams:int = 0
 sortedWord:str = sorted(word)       
 for item in wordList:
     sortedItem:str = sorted(item)
-    if len(item) == len(word):
-        if sortedItem == sortedWord:
+    if len(item) == len(word) or len(word)>len(item):
+        if isAnagram(sortedItem,sortedWord) == 1:
             print('matching word:',item)
             anagrams+=1
         else:
             continue
-    elif len(word)>len(item):
-        if isAnagram(sortedItem,sortedWord) == 1:
-           print('matching word:',item)
-           anagrams+=1
     else: 
         continue
 print('size of the word list is: ',len(wordList))                  
